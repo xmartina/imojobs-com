84fef49e4aba90f5b41f8ad511e23ae24f625220
unknown <martinsprince126@gmail.com>
2023-04-21 19:04:10 +0100

    added new breadcrumb to all inner pages

---
+++ breadcrumb-login.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="front_web/home/img/imo-fav.webp">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="front_web/home/css/style.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/solid.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.3/assets/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.3/assets/owl.theme.default.css" />
+    <title>Home | Imo Jobs</title>
+</head>
+<body>
+<div class="breadcrumb-section text-white py-5 position-relative">
+    <div class="position-absolute top-0-cus w-100 h-100"></div>
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-lg-7 col-md-6 h-100 d-flex align-items-center">
+                <div class="row align-items-center">
+                    <div class="col-3">
+                        <span class="material-symbols-outlined icon-reg rounded-circle p-4 mr-5">how_to_reg</span>
+                    </div>
+                    <div class="col-9">
+                        <div class="pl-1 ml-4 w-100">
+                            <div class="h2 w-100">Employer Login</div>
+                            <div class="lead d-flex">
+                                Home  <div class="text-white ml-2"> / Login</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-6">
+                <div class="row">
+                    <div class="col-lg-3 "></div>
+                    <div class="col-lg-9 d-flex justify-content-around ">
+                        <div class="py-2 px-4 btn text-white border-reg d-flex align-items-center justify-content-between" onclick="location.href=('/imojobs.com.ng');"><span class="material-symbols-outlined text-14 mr-2">arrow_back</span> Back to home</div>
+                        <div class="py-2 px-4 btn text-white bg-reg d-flex align-items-center justify-content-between" onclick="location.href=('../employer-register.php')">Register <span class="material-symbols-outlined text-14 ml-2">arrow_forward</span></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
